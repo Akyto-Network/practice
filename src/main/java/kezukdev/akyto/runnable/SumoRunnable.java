e701d6460a2abd4f18baf9feabed908f61eebe07
Victor <47831936+VictorEgret@users.noreply.github.com>
2024-05-05 20:30:29 +0200

    Fixed SumoRunnable.java NullPointerException

--- src/main/java/kezukdev/akyto/runnable/SumoRunnable.java
+++ src/main/java/kezukdev/akyto/runnable/SumoRunnable.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import kezukdev.akyto.duel.cache.DuelState;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -22,7 +23,7 @@ public class SumoRunnable extends BukkitRunnable {
     }
     
     public void run() {
-        if (match == null) {
+        if (match == null || match.getState().equals(DuelState.FINISHING)) {
             this.cancel();
             return;
         }
@@ -31,7 +32,7 @@ public class SumoRunnable extends BukkitRunnable {
         uuids.addAll(match.getSecond());
         for (final UUID uuid : uuids) {
         	final Player player = Bukkit.getPlayer(uuid);
-            if (player.getLocation().getBlock().isLiquid()) {
+            if (player != null && player.getLocation().getBlock().isLiquid()) {
                 this.cancel();
                 this.main.getManagerHandler().getDuelManager().endSingle(match.getFirst().contains(player.getUniqueId()) ? new ArrayList<>(match.getSecond()).get(0) : new ArrayList<>(match.getFirst()).get(0));
                 break;
